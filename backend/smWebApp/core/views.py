5e14ab46eb01760d60603710cd1fe6b6be4312c6
yacine <lyacine230@gmail.com>
2024-03-24 15:34:20 +0100

    fixing a bug

--- backend/smWebApp/core/views.py
+++ backend/smWebApp/core/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import authenticate, login, logout , User 
 from django.http import JsonResponse
 from django.shortcuts import render
 
